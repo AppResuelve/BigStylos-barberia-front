972840a06d94f6f1f8c40b26f91c893adeae0fc8
F.Fernandez <hellsingsd@gmail.com>
2024-01-22 15:31:47 -0300

    borrado logico Usuarios

--- src/components/users/users.jsx
+++ src/components/users/users.jsx
@@ -5,6 +5,7 @@ import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
 import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
 import { useMediaQueryHook } from "../interfazMUI/useMediaQuery";
+import DeleteSweepIcon from '@mui/icons-material/DeleteSweep';
 const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 const Users = () => {
@@ -29,7 +30,7 @@ const Users = () => {
     fetchData();
   }, [add]);
 
-  
+
   const handleUpdateUser = async (email) => {
     try {
       const response = await axios.put(`${VITE_BACKEND_URL}/users/update`, {
@@ -40,6 +41,16 @@ const Users = () => {
       console.error("Error al updatear usuario", error);
     }
   };
+
+  const handleDelete = async (email) => {
+    try {
+      console.log(email)
+      const response = await axios.put(`${VITE_BACKEND_URL}/users/delete`, {email});
+      setAdd(!add);
+    } catch (error) {
+      console.error("Error al updatear usuario", error);
+    }
+  }
 
   return (
     <div>
@@ -56,43 +67,73 @@ const Users = () => {
         />
       )}
       {!sm ? (
-        <Box style={{ display: "flex" }}>
-          <Box style={{ width: "50%" }}>
-            <h2>Profesionales</h2>
-            {allUsers.map(
-              (user, index) =>
-                allUsers.length > 0 &&
-                user &&
-                user.worker == true && (
-                  <Box key={index}>
-                    <h4>{user.name}</h4>
-                    <h4>{user.email}</h4>
-                    <Box
-                      //height: 400px;
-                      style={{
-                        display: "flex",
-                        justifyContent: "space-between",
-                      }}
-                    >
-                      <Button style={{ color: "red", borderRadius: "50px" }}>
-                        <DeleteOutlineIcon />
-                      </Button>
-                      <Button onClick={() => handleUpdateUser(user.email)}>
-                        <KeyboardDoubleArrowRightIcon />
-                      </Button>
+        <Box style={{ display: "flex", flexDirection: "column" }}>
+          <Box style={{ display: "flex" }}>
+            <Box style={{ width: "50%" }}>
+              <h2>Profesionales</h2>
+              {allUsers.map(
+                (user, index) =>
+                  allUsers.length > 0 &&
+                  user &&
+                  user.worker == true && (
+                    <Box key={index}>
+                      <h4>{user.name}</h4>
+                      <h4>{user.email}</h4>
+                      <Box
+                        //height: 400px;
+                        style={{
+                          display: "flex",
+                          justifyContent: "space-between",
+                        }}
+                      >
+                        <Button onClick={() => handleUpdateUser(user.email)}>
+                          <KeyboardDoubleArrowRightIcon />
+                        </Button>
+                      </Box>
+                      <hr />
                     </Box>
-                    <hr />
-                  </Box>
-                )
-            )}
+                  )
+              )}
+            </Box>
+
+            <Box style={{ width: "50%" }}>
+              <h2>Clientes</h2>
+              {allUsers.map(
+                (user, index) =>
+                  allUsers.length > 0 &&
+                  user &&
+                  user.worker == false && (
+                    <Box key={index}>
+                      <h4>{user.name}</h4>
+                      <h4>{user.email}</h4>
+                      <Box
+                        style={{
+                          display: "flex",
+                          justifyContent: "space-between",
+                        }}
+                      >
+                        <Button onClick={() => handleUpdateUser(user.email)}>
+                          <KeyboardDoubleArrowLeftIcon />
+                        </Button>
+                        <Button onClick={() => handleDelete(user.email)} style={{ color: "red", borderRadius: "50px" }}>
+                          <DeleteOutlineIcon />
+                        </Button>
+                      </Box>
+
+                      <hr />
+                    </Box>
+                  )
+              )}
+            </Box>
           </Box>
-          <Box style={{ width: "50%" }}>
-            <h2>Clientes</h2>
+
+          <Box style={{ width: "100%" }}>
+            <h2>Eliminados</h2>
             {allUsers.map(
               (user, index) =>
                 allUsers.length > 0 &&
                 user &&
-                user.worker == false && (
+                user.isDelete == true && (
                   <Box key={index}>
                     <h4>{user.name}</h4>
                     <h4>{user.email}</h4>
@@ -102,11 +143,9 @@ const Users = () => {
                         justifyContent: "space-between",
                       }}
                     >
-                      <Button onClick={() => handleUpdateUser(user.email)}>
-                        <KeyboardDoubleArrowLeftIcon />
-                      </Button>
-                      <Button style={{ color: "red", borderRadius: "50px" }}>
-                        <DeleteOutlineIcon />
+
+                      <Button onClick={() => handleDelete(user.email)} style={{ color: "orange", borderRadius: "50px" }}>
+                        <DeleteSweepIcon />
                       </Button>
                     </Box>
 
@@ -119,98 +158,126 @@ const Users = () => {
       ) : (
         /* ///// sección users para mobile ///// */
         <Box>
-          <Box style={{ width: "100%" }}>
-            <Button
-              variant={typeUser ? "contained" : "outlined"}
-              style={{
-                width: "50%",
-                fontFamily: "jost, sans-serif",
-                fontWeight: "bold",
-                border: "none",
-                borderBottom: !typeUser ? "3px solid" : "3px solid #2196f3",
-                borderRadius: typeUser
-                  ? "20px 20px 0px 0px"
-                  : "5px 5px 0px 0px",
-              }}
-              onClick={() => setTypeUser(true)}
-            >
-              Profesionales
-            </Button>
-            <Button
-              variant={!typeUser ? "contained" : "outlined"}
-              style={{
-                width: "50%",
-                fontFamily: "jost, sans-serif",
-                fontWeight: "bold",
-                border: "none",
-                borderBottom: typeUser ? "3px solid" : "3px solid #2196f3",
-                borderRadius: !typeUser
-                  ? "20px 20px 0px 0px"
-                  : "5px 5px 0px 0px",
-              }}
-              onClick={() => setTypeUser(false)}
-            >
-              Clientes
-            </Button>
+          <Box>
+            <Box style={{ width: "100%" }}>
+              <Button
+                variant={typeUser ? "contained" : "outlined"}
+                style={{
+                  width: "50%",
+                  fontFamily: "jost, sans-serif",
+                  fontWeight: "bold",
+                  border: "none",
+                  borderBottom: !typeUser ? "3px solid" : "3px solid #2196f3",
+                  borderRadius: typeUser
+                    ? "20px 20px 0px 0px"
+                    : "5px 5px 0px 0px",
+                }}
+                onClick={() => setTypeUser(true)}
+              >
+                Profesionales
+              </Button>
+              <Button
+                variant={!typeUser ? "contained" : "outlined"}
+                style={{
+                  width: "50%",
+                  fontFamily: "jost, sans-serif",
+                  fontWeight: "bold",
+                  border: "none",
+                  borderBottom: typeUser ? "3px solid" : "3px solid #2196f3",
+                  borderRadius: !typeUser
+                    ? "20px 20px 0px 0px"
+                    : "5px 5px 0px 0px",
+                }}
+                onClick={() => setTypeUser(false)}
+              >
+                Clientes
+              </Button>
+            </Box>
+            <Box style={{ width: "100%" }}>
+              {
+                typeUser
+                  ? allUsers.length > 0 &&
+                  allUsers.map((user, index) => {
+                    if (user.worker) {
+                      return (
+                        <Box key={index} style={{ marginTop: "18px" }}>
+                          <h4>{user.name}</h4>
+                          <h4>{user.email}</h4>
+                          <Box
+                            style={{
+                              display: "flex",
+                              justifyContent: "space-between",
+                            }}
+                          >
+                            <Button onClick={() => handleUpdateUser(user.email)}>
+                              <KeyboardDoubleArrowRightIcon />
+                            </Button>
+                          </Box>
+                          <hr />
+                        </Box>
+                      );
+                    }
+                  })
+                  : allUsers.length > 0 &&
+                  allUsers.map((user, index) => {
+                    if (!user.worker) {
+                      return (
+                        <Box key={index} style={{ marginTop: "18px" }}>
+                          <h4>{user.name}</h4>
+                          <h4>{user.email}</h4>
+                          <Box
+                            style={{
+                              display: "flex",
+                              justifyContent: "space-between",
+                            }}
+                          >
+                            <Button onClick={() => handleUpdateUser(user.email)}>
+                              <KeyboardDoubleArrowRightIcon />
+                            </Button>
+                            <Button
+                              style={{ color: "red", borderRadius: "50px" }}
+                            >
+                              <DeleteOutlineIcon />
+                            </Button>
+                          </Box>
+                          <hr />
+                        </Box>
+                      );
+                    }
+                  })}
+
+            </Box>
+
           </Box>
           <Box style={{ width: "100%" }}>
-            {typeUser
-              ? allUsers.length > 0 &&
-                allUsers.map((user, index) => {
-                  if (user.worker) {
-                    return (
-                      <Box key={index} style={{ marginTop: "18px" }}>
-                        <h4>{user.name}</h4>
-                        <h4>{user.email}</h4>
-                        <Box
-                          style={{
-                            display: "flex",
-                            justifyContent: "space-between",
-                          }}
-                        >
-                          <Button onClick={() => handleUpdateUser(user.email)}>
-                            <KeyboardDoubleArrowRightIcon />
-                          </Button>
-                          <Button
-                            style={{ color: "red", borderRadius: "50px" }}
-                          >
-                            <DeleteOutlineIcon />
-                          </Button>
-                        </Box>
-                        <hr />
-                      </Box>
-                    );
-                  }
-                })
-              : allUsers.length > 0 &&
-                allUsers.map((user, index) => {
-                  if (!user.worker) {
-                    return (
-                      <Box key={index} style={{ marginTop: "18px" }}>
-                        <h4>{user.name}</h4>
-                        <h4>{user.email}</h4>
-                        <Box
-                          style={{
-                            display: "flex",
-                            justifyContent: "space-between",
-                          }}
-                        >
-                          <Button onClick={() => handleUpdateUser(user.email)}>
-                            <KeyboardDoubleArrowRightIcon />
-                          </Button>
-                          <Button
-                            style={{ color: "red", borderRadius: "50px" }}
-                          >
-                            <DeleteOutlineIcon />
-                          </Button>
-                        </Box>
-                        <hr />
-                      </Box>
-                    );
-                  }
-                })}
+            <h2>Eliminados</h2>
+            {allUsers.map(
+              (user, index) =>
+                allUsers.length > 0 &&
+                user &&
+                user.isDelete == true && (
+                  <Box key={index}>
+                    <h4>{user.name}</h4>
+                    <h4>{user.email}</h4>
+                    <Box
+                      style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                      }}
+                    >
+
+                      <Button onClick={() => handleDelete(user.email)} style={{ color: "orange", borderRadius: "50px" }}>
+                        <DeleteSweepIcon />
+                      </Button>
+                    </Box>
+
+                    <hr />
+                  </Box>
+                )
+            )}
           </Box>
         </Box>
+
       )}
     </div>
   );
