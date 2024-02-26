d4c64863d183b7a56eaf1431af2a21dfb6de3031
tomascallenius <tomascallenius96@gmail.com>
2024-02-25 21:38:10 -0300

    fondo img default

--- src/components/turns/turns.jsx
+++ src/components/turns/turns.jsx
@@ -5,7 +5,7 @@ import ShowTurns from "../showTurns/showTurns";
 import { useMediaQueryHook } from "../interfazMUI/useMediaQuery";
 import { Box, Button } from "@mui/material";
 import calendar from "../../assets/images/calendar2.png";
-import defaultServiceImg from "../../assets/images/banner-de-peluqueria-fondo-horizontal.jpg";
+import defaultServiceImg from "../../assets/images/serviciosfondodefault.png";
 import formatHour from "../../functions/formatHour";
 import axios from "axios";
 import "./turns.css";
