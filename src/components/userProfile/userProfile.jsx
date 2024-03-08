8c9b15e17d552e31925f4d038505dfedaa15e549
tomascallenius <tomascallenius96@gmail.com>
2024-03-08 19:02:00 -0300

    asd

--- src/components/userProfile/userProfile.jsx
+++ src/components/userProfile/userProfile.jsx
@@ -6,6 +6,7 @@ import noUser from "../../assets/icons/noUser.png";
 import ModalMUI from "../interfazMUI/userModal";
 import "./userProfile.css";
 
+
 const Profile = ({ userData }) => {
   const { darkMode } = useContext(DarkModeContext);
   const { user, isAuthenticated, isLoading } = useAuth0();
