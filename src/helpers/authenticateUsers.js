6c41685e6a0979fd175e713b1e79994782bb0b7b
tomascallenius <tomascallenius96@gmail.com>
2024-07-17 23:08:52 -0300

    asd

--- src/helpers/authenticateUsers.js
+++ src/helpers/authenticateUsers.js
@@ -1,28 +1,29 @@
-import { getCookie} from "./cookies";
+import { getCookie } from "./cookies";
 import axios from "axios";
 
 const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 const authenticateUsers = async () => {
   const idSession = getCookie("IDSESSION");
- let objResult = {
-   user: {},
-   auth: false,
- };
+  let objResult = {
+    user: {},
+    auth: false,
+  };
+  console.log(idSession);
   if (idSession) {
     try {
       const response = await axios.post(`${VITE_BACKEND_URL}/users/byemail`, {
         email: idSession,
       });
+      console.log(response.data);
       objResult.user = response.data;
       objResult.auth = true;
     } catch (error) {
       console.log(error);
     }
   } else {
-    
   }
   return objResult;
 };
 
-export default authenticateUsers
+export default authenticateUsers;
